989a79f57408943382d1858aa65cf4192e8e621f
John Richards <johnprichards3@gmail.com>
2018-03-19 15:44:19 -0400

    add all iOS/tvOS urls

--- xmlscan.py
+++ xmlscan.py
@@ -4,7 +4,10 @@ import plistlib
 import requests
 
 ioscatalog = 'http://mesu.apple.com/assets/com_apple_MobileAsset_SoftwareUpdate/com_apple_MobileAsset_SoftwareUpdate.xml'
+iospublicseed = 'http://mesu.apple.com/assets/iOSPublicSeed/com_apple_MobileAsset_SoftwareUpdate/com_apple_MobileAsset_SoftwareUpdate.xml'
+ios11publicseed = 'http://mesu.apple.com/assets/iOS11PublicSeed/com_apple_MobileAsset_SoftwareUpdate/com_apple_MobileAsset_SoftwareUpdate.xml'
 tvoscatalog = 'http://mesu.apple.com/assets/tv/com_apple_MobileAsset_SoftwareUpdate/com_apple_MobileAsset_SoftwareUpdate.xml'
+tvos11publicseed = 'http://mesu.apple.com/assets/tvOS11PublicSeed/com_apple_MobileAsset_SoftwareUpdate/com_apple_MobileAsset_SoftwareUpdate.xml'
 
 def parse(catalog):
     builds = {}
@@ -24,6 +27,12 @@ def main():
     print parse(ioscatalog) + '\n'
     print "Fetching tvOS Builds from %s" % tvoscatalog
     print parse(tvoscatalog) + '\n'
+    print "Fetching iOS Public Seed Builds from %s" % iospublicseed
+    print parse(iospublicseed) + '\n'
+    print "Fetching iOS 11 Public Seed Builds from %s" % ios11publicseed
+    print parse(ios11publicseed) + '\n'
+    print "Fetching tvOS 11 Public Seed Builds from %s" % tvos11publicseed
+    print parse(tvos11publicseed) + '\n'
 
 if __name__ == '__main__':
     main()
